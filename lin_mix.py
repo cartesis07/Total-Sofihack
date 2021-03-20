e5898f42bc21ba0c6d8ce7d8daf8eab6ea56e91a
Victor MASIAK <victor.masiak@gmail.com>
2021-03-21 00:41:20 +0100

    v2 : data is coherent but shifted

--- lin_mix.py
+++ lin_mix.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 inputs_path = "E:\\inputs"
 outputs_path = "E:\\outputs"
 
-headers = ['# Consommation (MW)', 'Thermique (MW)', 'Nucléaire (MW)', 'Eolien (MW)', 'Solaire (MW)', 'Hydraulique (MW)', 'Pompage (MW)', 'Bioénergies (MW)']
+headers = ['# Consommation (MW)', 'Thermique (MW)', 'Nucléaire (MW)', 'Eolien (MW)', 'Solaire (MW)', 'Hydraulique (MW)', 'Pompage (MW)', 'Bioénergies (MW)', 'Ech. physiques (MW)']
 previous = ['date'] + headers
 
 code_region = 52
@@ -45,10 +45,13 @@ for index, row in df.iterrows():
             delta_time_sec = 24*3600*delta_time.days + delta_time.seconds
             row['reg_a_' + p] = (row[p] - row['previous_' + p]) / delta_time_sec
             row['reg_b_' + p] = row[p] - row['reg_a_' + p] * datetime.strptime(row['date'], '%Y-%m-%d %H:%M:%S').timestamp()
+        else:
+            row['reg_a_' + p] = 10**99
+            row['reg_b_' + p] = 10**99
         previous_val[p] = row[p]
-    new_df = new_df.append(row,ignore_index=True)
+    new_df = new_df.append(row, ignore_index=True)
 
-R = list(df.iterrows())
+R = list(new_df.iterrows())
 
 date_debut = datetime.strptime('2020-11-14 00:00:00', '%Y-%m-%d %H:%M:%S')
 date_fin = datetime.strptime('2020-11-16 23:30:00', '%Y-%m-%d %H:%M:%S')
@@ -58,20 +61,25 @@ while date_ar[-1] != date_fin:
     date_ar.append(date_ar[-1] + timedelta(minutes=30))
 
 result = pd.DataFrame()
-current_row = 0
+current_row = 1
 r_date = datetime.strptime(R[current_row][1]['date'], '%Y-%m-%d %H:%M:%S')
 for d in date_ar:
-    while (d - r_date).seconds > 0:
+    delta_time = d - r_date
+    delta_time_sec = 24*3600*delta_time.days + delta_time.seconds
+    while delta_time_sec > 0:
         current_row += 1
         r_date = datetime.strptime(R[current_row][1]['date'], '%Y-%m-%d %H:%M:%S')
-        result_line = dict()
-        for h in headers:
-            a = R[current_row][1]['reg_a_' + p]
-            b = R[current_row][1]['reg_b_' + p]
-            t = d.timestamp()
-            r = a*t+b
-            result_line['date'] = d
-            result_line['predicted_' + h] = r
-        result = result.append(result_line, ignore_index=True)
+        delta_time = d - r_date
+        delta_time_sec = 24*3600*delta_time.days + delta_time.seconds
+    result_line = pd.Series()
+    result_line['date'] = d
+    result_line['current_row'] = current_row
+    for h in headers:
+        a = R[current_row][1]['reg_a_' + h]
+        b = R[current_row][1]['reg_b_' + h]
+        t = d.timestamp()
+        r = a*t+b
+        result_line['predicted_' + h] = r
+    result = result.append(result_line, ignore_index=True)
 
 result.to_csv("E:\\mix_lin_" + str(code_region) + ".csv")
