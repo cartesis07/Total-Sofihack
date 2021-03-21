7710ea77fba522fd34819f561b169865f42240fa
cartesis07 <cartesis07@gmail.com>
2021-03-21 15:43:49 +0100

    model optimization

--- predict_mix.py
+++ predict_mix.py
@@ -11,6 +11,7 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from tensorflow import keras
 from tensorflow.keras import layers
+from keras import regularizers
 import math
 import os
 
@@ -45,39 +46,48 @@ dense_layer_2 = layers.Dense(50, activation='relu')(dense_layer_1)
 dense_layer_3 = layers.Dense(25, activation='relu')(dense_layer_2)
 output = layers.Dense(9)(dense_layer_3)
 
+# input_layer = keras.Input(shape=(X.shape[1],))
+# dense_layer_1 = layers.Dense(200, activation='relu', kernel_regularizer=regularizers.l2(), bias_regularizer=regularizers.l2())(input_layer)
+# drop_out_layer_1 = layers.Dropout(0.5)(dense_layer_1)
+# dense_layer_2 = layers.Dense(100, activation='relu', kernel_regularizer=regularizers.l2(), bias_regularizer=regularizers.l2())(drop_out_layer_1)
+# drop_out_layer_2 = layers.Dropout(0.5)(dense_layer_2)
+# dense_layer_3 = layers.Dense(50, activation='relu', kernel_regularizer=regularizers.l2(), bias_regularizer=regularizers.l2())(drop_out_layer_2)
+# drop_out_layer_3 = layers.Dropout(0.5)(dense_layer_3)
+# output = layers.Dense(9)(drop_out_layer_3)
+
 model = keras.Model(inputs=input_layer, outputs=output)
 model.compile(loss="mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
 
-history = model.fit(X_train, y_train, batch_size=2, epochs=10, verbose=1, validation_split=0.2, validation_data=(X_test,y_test))
+history = model.fit(X_train, y_train, batch_size=2, epochs=100, verbose=1, validation_split=0.2, validation_data=(X_test,y_test))
 
 model.evaluate(X_test, y_test, verbose=1)
 
-folder_2019 = "Hackathon_Data/Dataset/Prediction1/Inputs/2019"
+# folder_2019 = "Hackathon_Data/Dataset/Prediction1/Inputs/2019"
 
-folder_2020 = "Hackathon_Data/Dataset/Prediction1/Inputs/2020"
+# folder_2020 = "Hackathon_Data/Dataset/Prediction1/Inputs/2020"
 
-count = 0
-for subdir, dirs, files in os.walk(folder_2019):
-    for file in files:
-        count += 1
-        filepath = subdir + os.sep + file
-        tmp = pd.read_csv(filepath,usecols=inputs_list)
-        tmp_X = tmp.iloc[:, 0:55].values
-        tmp_X = sc.fit_transform(tmp_X)
-        pred = model.predict(tmp_X)
-        np.savetxt("Hackathon_Data/Dataset/Prediction1/Outputs/" + 'output' + file, pred, header="Consommation (MW),Thermique (MW),Nucléaire (MW),Eolien (MW),Solaire (MW),Hydraulique (MW),Pompage (MW),Bioénergies (MW),Ech. physiques (MW)", delimiter=',')
+# count = 0
+# for subdir, dirs, files in os.walk(folder_2019):
+#     for file in files:
+#         count += 1
+#         filepath = subdir + os.sep + file
+#         tmp = pd.read_csv(filepath,usecols=inputs_list)
+#         tmp_X = tmp.iloc[:, 0:55].values
+#         tmp_X = sc.fit_transform(tmp_X)
+#         pred = model.predict(tmp_X)
+#         np.savetxt("Hackathon_Data/Dataset/Prediction2/Outputs/" + 'output' + file, pred, header="Consommation (MW),Thermique (MW),Nucléaire (MW),Eolien (MW),Solaire (MW),Hydraulique (MW),Pompage (MW),Bioénergies (MW),Ech. physiques (MW)", delimiter=',')
 
-count = 0
-for subdir, dirs, files in os.walk(folder_2020):
-    for file in files:
-        count += 1
-        filepath = subdir + os.sep + file
-        tmp = pd.read_csv(filepath,usecols=inputs_list)
-        tmp_X = tmp.iloc[:, 0:55].values
-        tmp_X = sc.fit_transform(tmp_X)
-        pred = model.predict(tmp_X)
-        np.savetxt("Hackathon_Data/Dataset/Prediction1/Outputs/" + "output" + file , pred, header="Consommation (MW),Thermique (MW),Nucléaire (MW),Eolien (MW),Solaire (MW),Hydraulique (MW),Pompage (MW),Bioénergies (MW),Ech. physiques (MW)", delimiter=',')
-        #with open("Hackathon_Data/Dataset/Prediction1/Outputs/2020/" + "pred" + str(count) + '.txt', 'w') as filehandle:
-        #    for listitem in pred:
-        #        filehandle.write('%s\n' % listitem)
+# count = 0
+# for subdir, dirs, files in os.walk(folder_2020):
+#     for file in files:
+#         count += 1
+#         filepath = subdir + os.sep + file
+#         tmp = pd.read_csv(filepath,usecols=inputs_list)
+#         tmp_X = tmp.iloc[:, 0:55].values
+#         tmp_X = sc.fit_transform(tmp_X)
+#         pred = model.predict(tmp_X)
+#         np.savetxt("Hackathon_Data/Dataset/Prediction2/Outputs/" + "output" + file , pred, header="Consommation (MW),Thermique (MW),Nucléaire (MW),Eolien (MW),Solaire (MW),Hydraulique (MW),Pompage (MW),Bioénergies (MW),Ech. physiques (MW)", delimiter=',')
+#         #with open("Hackathon_Data/Dataset/Prediction1/Outputs/2020/" + "pred" + str(count) + '.txt', 'w') as filehandle:
+#         #    for listitem in pred:
+#         #        filehandle.write('%s\n' % listitem)
 
